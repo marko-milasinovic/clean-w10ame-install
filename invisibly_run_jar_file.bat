3b43a9d071390ffc948445ea116cb4a9f0432625
marko-milasinovic <72104952+marko-milasinovic@users.noreply.github.com>
2021-09-10 21:47:42 +0200

    Update invisibly_run_jar_file.bat

--- invisibly_run_jar_file.bat
+++ invisibly_run_jar_file.bat
@@ -1,1 +1,1 @@
-cmd.exe /c echo CreateObject("Wscript.Shell").Run "C:\MyPrograms\%PATH_TO_PROGRAM%\%PROGRAM_NAME.bat%", 0, false > run_hidden.vbs & start /wait wscript run_hidden.vbs & del run_hidden.vbs
+cmd.exe /c echo CreateObject("Wscript.Shell").Run "C:\%PATH_TO_PROGRAM%\%PROGRAM_NAME%", 0, false > run_hidden.vbs & start /wait wscript run_hidden.vbs & del run_hidden.vbs
